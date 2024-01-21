167aeea0360884673bd952922c9fa9dbc8eec2f2
Charles Wu <charlw5@uci.edu>
2024-01-20 22:22:18 -0800

    Refactored and added argparser

--- pts-troll.py
+++ pts-troll.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
 import os
 import sys
+import argparse
 
 def get_own_tty():
     tty_name = os.ttyname(sys.stdout.fileno())
     tty_num = tty_name.split("/")[3]
     return tty_num
 
-def main():
+def deploy_troll():
+
     own_tty = get_own_tty()
     other_tty = [tty for tty in os.listdir("/dev/pts") if tty not in [own_tty, "ptmx"]]
     for tty_num in other_tty:
-        os.system("cat duck_ascii.txt > /dev/pts/{}".format(tty_num))
+        os.system("cat assets/duck_ascii.txt > /dev/pts/{}".format(tty_num))
     #print(other_tty)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.parse_args()
+
+    deploy_troll()
 
 
 
